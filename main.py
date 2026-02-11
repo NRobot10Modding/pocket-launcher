640d167dc1669ed349744a4d8c7034647f3e45ef
nrobot10modding <nathanr11.ingham@outlook.com>
2026-02-11 22:44:29 +0000

    Second Commit

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ import subprocess
 
 mc_dir = './minecraft/'
 all_ver_info = mc.utils.get_available_versions(mc_dir)
+
+ver_list = []
+av_ver_list = []
+
+def install_version():
+    for version in all_ver_info:
+        ver_list.append(version["id"])
+
+    print(ver_list)
+
+install_version()
